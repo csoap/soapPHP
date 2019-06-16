b910307bb0c93f089158c109ac36966cdad4a396
蔡遵造 <caizunzao@4399inc.com>
2019-06-16 17:07:26 +0800

    composer新增  dump输出：symfony/var-dumper，报错提醒： filp/whoops

--- core/lib/drive/log/file.php
+++ core/lib/drive/log/file.php
@@ -18,6 +18,6 @@ class file
         if(!is_dir($this->path.date('YmdH'))){
             mkdir($this->path.date('YmdH'), '0777', true);
         }
-        return file_put_contents($this->path.date('YmdH').'/'. $file.'.php', date('Y-m-d H:i:s').json_encode($message).PHP_EOL, FILE_APPEND);
+        return file_put_contents($this->path.date('YmdH').'/'. $file.'.log', date('Y-m-d H:i:s').json_encode($message).PHP_EOL, FILE_APPEND);
     }
 }
--- index.php
+++ index.php
@@ -5,7 +5,15 @@ define('API', APP_PATH.'/api');
 define('MODULE', 'api');
 define('DEBUG', true);
 
+include "vendor/autoload.php";
+
 if(DEBUG){
+    $whoops = new \Whoops\Run;
+    $errTitle = '框架出错';
+    $option = new \Whoops\Handler\PrettyPageHandler();
+    $option->setPageTitle($errTitle);
+    $whoops->pushHandler($option);
+    $whoops->register();
     ini_set('display_errors', 'On');
 }else{
     ini_set('display_errors', 'Off');
